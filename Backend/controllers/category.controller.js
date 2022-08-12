fb1b3f75035110c4c25d4a26ff90706c1b8dc198
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-12 09:36:54 -0500

    created controller for category

---
+++ Backend/controllers/category.controller.js
@@ -0,0 +1,62 @@
+const { JSONResponse } = require("../lib/helper");
+const Category = require("../models/category.model");
+
+class CategoryController{
+    /**
+     * ### Description
+     * Gets all the categories that were saved in the database.
+     */
+    static getAllCategories = async (req, res, next)=>{
+        try{
+            const categories = await Category.find()
+            JSONResponse.success(res, "Success", categories, 200)
+        }catch(error){
+            JSONResponse.error(res, "Failed", error, 404)
+        }
+    }
+
+        /**
+     * ### Description
+     * Gets a single category based on the id that was passed in.
+     */
+    static getCategoryById = async (req, res, next)=>{
+        try{
+            const id = req.params.id;
+            const category = await Category.findById(id)
+            JSONResponse.success(res, "Success", category, 200)
+        }catch(error){
+            JSONResponse.error(res, "Failed", error, 404)
+        }
+    }
+
+        /**
+     * ### Description
+     * Updates the category that matches the id that was passed to the route.
+     */
+    static updateCategory = async (req,res, next)=>{
+        try{
+            const id = req.params.id;
+            const category = await Category.findByIdAndUpdate(id, req.body, {new: true});
+            JSONResponse.success(res, "Success", category, 200)
+        }catch(error){
+            JSONResponse.error(res, "Success", error, 404);
+        }
+    }
+
+        /**
+     * ### Description
+     * Deletes the category from the database.
+     */
+    static DeleteCategory = async (req,res, next)=>{
+        try{
+            const id = req.params.id;
+            await Category.findByIdAndDelete(id);
+            JSONResponse.success(res, "Success", category, 200)
+        }catch(error){
+            JSONResponse.error(res, "Success", error, 404);
+        }
+    }
+}
+
+
+module.exports = CategoryController;
