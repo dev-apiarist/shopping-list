832909d633b8268ce6f4664d2b9d7ee9bdbba003
aldaine-AG <aldaineclarke1@gmail.com>
2022-08-12 09:15:40 -0500

    updated item schema to refer to the category schema

--- Backend/models/items.model.js
+++ Backend/models/items.model.js
@@ -1,7 +1,7 @@
 const db = require('mongoose')
 
 let itemsSchema = new db.Schema({
-	category: { type: String, required: true },
+	category: { type: db.Schema.Types.ObjectId, required: true , ref:"Category"},
 	item_name: { type: String, required: true },
 	price: { type: Number, required: true },
 	quantity: { type: Number, required: true },
